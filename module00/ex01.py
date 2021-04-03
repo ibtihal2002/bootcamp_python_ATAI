8f96d55719afa091ad34e7f6d05afdf99948dc23
ibtihal2002 <80362007+ibtihal2002@users.noreply.github.com>
2021-04-03 20:57:08 +0100

    Update and rename ex00.py to ex01.py

---
+++ module00/ex01.py
@@ -0,0 +1,12 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  3 11:25:02 2021
+@author: IBTIHAL
+"""
+
+def reverse(text):
+    space =''
+    for char in text:
+        space = char + space
+    return space
