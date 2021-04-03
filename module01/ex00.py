dbb84fbc4ada2278b31ab0a5d1bba25b2de97613
ibtihal2002 <80362007+ibtihal2002@users.noreply.github.com>
2021-04-03 21:04:24 +0100

    Update ex00.py

--- module01/ex00.py
+++ module01/ex00.py
@@ -1,1 +1,28 @@
 
+class book:
+    recipes_list={"starter":" ","lunch": " ","desert":" ",}
+    name="0"
+    last_update="0"
+    creation_date="0"
+    def __str__(self,name,last_update,creation_date,recipes_list):
+        self.name = name
+        self.last_update = last_update 
+        self.creation_date = creation_date
+        self.recipes_list = recipes_list
+        
+    def get_recipe_by_name(self, name):
+        pass
+        for key in self.recipes_list:
+            for recipe in self.recipes_list[key]:
+                if recipe.name==name:
+                    print(recipe)
+                
+        
+
+    def get_recipes_by_types(self, recipe_type):
+        """Get all recipe names for a given recipe_type """
+        pass
+
+    def add_recipe(self, recipe):
+        """Add a recipe to the book and update last_update"""
+        pass
