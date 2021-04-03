c1ac570138527f251b2984ac31fe288ccbb919c3
ibtihal2002 <80362007+ibtihal2002@users.noreply.github.com>
2021-04-03 20:59:01 +0100

    Add files via upload

---
+++ module00/exo_5.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  3 11:39:37 2021
+
+@author: IBTIHAL
+"""
+#kata00
+def tpl(t):
+    n=len(t)
+    print("the",n," numbers are:",end='')
+    for i in range (0,n):
+        print(t[i],end=',')
+        
+#kata01
+languages = {
+    'Python': 'Guido van Rossum',
+    'Ruby': 'Yukihiro Matsumoto',
+    'PHP': 'Rasmus Lerdorf',
+    }
+for key,value in languages.items():
+    print(key," was created by ",value)
+
+#kata02
+def timing(hour, minutes, year, month, day):
+    print(month,"/",day,"/",year," ",hour,":",minutes)
+    
+#kata03
+def kata03(phrase):
+    print(phrase.rjust(42,"-"))
+    
+#kata04
+"{:2e}".format( 0, 4, 132.42222, 10000, 12345.67)
+    
---
+++ module00/exo_8.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  3 11:47:39 2021
+
+@author: IBTIHAL
+"""
+
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+   'C':'-.-.', 'D':'-..', 'E':'.',
+   'F':'..-.', 'G':'--.', 'H':'....',
+   'I':'..', 'J':'.---', 'K':'-.-',
+   'L':'.-..', 'M':'--', 'N':'-.',
+   'O':'---', 'P':'.--.', 'Q':'--.-',
+   'R':'.-.', 'S':'...', 'T':'-',
+   'U':'..-', 'V':'...-', 'W':'.--',
+   'X':'-..-', 'Y':'-.--', 'Z':'--..',
+   '1':'.----', '2':'..---', '3':'...--',
+   '4':'....-', '5':'.....', '6':'-....',
+   '7':'--...', '8':'---..', '9':'----.',
+   '0':'-----', ', ':'--..--', '.':'.-.-.-',
+   '?':'..--..', '/':'-..-.', '-':'-....-',
+   '(':'-.--.', ')':'-.--.-'
+}
+import string
+def encryption(message):
+   new = ''
+   for letter in message:
+      if letter in string.punctuation:
+            return "ERROR"
+      if letter != ' ':
+         new += MORSE_CODE_DICT[letter]
+   return new
