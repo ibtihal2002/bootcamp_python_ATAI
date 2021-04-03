6e065e1484ff0c4d5419fc7ba5ef4f6dae77d5db
ibtihal2002 <80362007+ibtihal2002@users.noreply.github.com>
2021-04-03 12:40:32 +0100

    Add files via upload

---
+++ exo_7.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  3 11:39:37 2021
+
+@author: IBTIHAL
+"""
+
+def tpl(t):
+    n=len(t)
+    print("the",n," numbers are:",end='')
+    for i in range (0,n):
+        print(t[i],end=',')
