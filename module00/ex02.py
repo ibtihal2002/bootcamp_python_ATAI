6ccd4708adcb70eaaf4a8fcd1bd04b095dd0c5d2
ibtihal2002 <80362007+ibtihal2002@users.noreply.github.com>
2021-04-03 20:57:55 +0100

    Create ex02.py

---
+++ module00/ex02.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  3 11:44:44 2021
+@author: IBTIHAL
+"""
+
+def number(nb):
+    if nb==0:
+        print("I'm Zero.")
+    if nb%2==0:
+        print("I'm Even.")
+    else:
+        print ("I'm Odd.")
